39f65ba9b847668bcd0819e2110e5bd2371cc090
Joanna Wood <caitlinjoannawood@gmail.com>
2019-07-03 14:11:16 -0400

    ws sends db and new message with id to client

---
+++ message-db.js
@@ -0,0 +1,12 @@
+const messages = [{
+    username: 'Bob',
+    content: 'Has anyone seen my marbles?',
+    messageId: 1
+  },
+  {
+    username: 'Anonymous',
+    content: 'No, I think you lost them. You lost your marbles Bob. You lost them for good.',
+    messageId: 2
+  }
+];
+module.exports = messages;
--- server.js
+++ server.js
@@ -1,35 +1,57 @@
 // server.js
 const express = require('express');
 const SocketServer = require('ws').Server;
-
+const uuid = require('uuid/v1');
+const  messages  = require('./message-db.js')
 // Set the port to 3001
 const PORT = 3001;
-
 // Create a new express server
 const server = express()
   // Make the express server serve static assets (html, javascript, css) from the /public folder
   .use(express.static('public'))
   .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${PORT}`));
-
 // Create the WebSockets server
 const wss = new SocketServer({ server });
-
 // Set up a callback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
 // the ws parameter in the callback.
+
+const clients = [];
+
+function addMessageToDb(newMessage) {
+  messages.push(newMessage);
+  console.log('message db: ', messages);
+}
+
+SocketServer.prototype.broadcast = (msg) => {
+  clients.map((c) => {
+    c.send(JSON.stringify(msg));
+  });
+}
+
+
 wss.on('connection', (client) => {
+
+  clients.push(client);
+
+  wss.broadcast({ initialLoad: messages });
+
 
   client.on('message', (msgData) => {
     const msg = JSON.parse(msgData);
-    const {username}  = msg;
-    const {newMessage} = msg;
-    console.log('user: ', username);
-    console.log('content: ', newMessage);
+    const { username, content }  = msg;
+    const message = {
+      username: username,
+      content: content,
+      messageId: uuid()
+    };
+    addMessageToDb(message);
+    wss.broadcast(msg);
   }); 
 
-  // Set up a callback for when a client closes the socket. This usually means they closed their browser.
-  client.on('close', () => console.log('Client disconnected'));
 
+
+  client.on('close', () => console.log('Client disconnected'));
 });
 
 
